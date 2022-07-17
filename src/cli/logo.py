45a470e3f1fbd1d973d92bc7d8e63a024d3fda19
Matthew Stachyra <matthew.stachyra@tufts.edu>
2022-07-17 01:23:42 -0400

    README update

--- src/cli/logo.py
+++ src/cli/logo.py
@@ -2,8 +2,8 @@ from pyfiglet import Figlet
 
 class Logo:
     def __init__(self):
-        f = Figlet(font='slant')
-        self.logo = f.renderText('NAVA') + 'smart notetaking.' + '\n' + '\n' +'\n'
+        f = Figlet(font='rev')
+        self.logo = f.renderText('NAVA') + '\n' +  'smart notes, organized for you by NAVA' + '\n' + 'v0.1.0' +'\n'
 
     def __str__(self):
         print(self.logo)
